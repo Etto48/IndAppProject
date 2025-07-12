4b9252f4bbbbbb17f6bb3cba6a397114d63cb67c
Etto48 <ettore.ricci.coding@gmail.com>
2025-07-12 16:10:07 +0200

    Added custom favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "TravelMate",
   description: "Find the best hotels near you",
+  icons: "/favicon.svg"
 };
 
 export default function RootLayout({
